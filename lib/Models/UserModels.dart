af8a0515c4675a3672617e674c63d6514e2af70c
oozuser01 <walaaabohable953@gmail.com>
2024-10-26 11:29:16 +0300

    modelsvmhg

--- lib/Models/UserModels.dart
+++ lib/Models/UserModels.dart
@@ -4,21 +4,25 @@ class User{
     this.name="",
     this.phone="",
     this.note="",
+    this.adress=""
 });
   String id;
   String name;
   String phone;
   String note;
+  String adress;
   factory User.fromJson(Map<String, dynamic>json)=> User(
     id: json["id"],
-    name: json["id"],
-    phone: json["id"],
-    note: json["id"],
+    name: json["name"],
+    phone: json["phone"],
+    note: json["note"],
+    adress: json["adress"],
   );
   Map<String, dynamic> toJson() => {
     "id": id,
     "name": name,
     "phone": phone,
     "note": note,
+    "adress": adress,
   };
 }
